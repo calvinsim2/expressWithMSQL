7511950d75561e09e3a0b88e8fd910f2240a4f76
calvinsim2 <88623537+calvinsim2@users.noreply.github.com>
2022-09-05 22:41:39 +0800

    PROGRESS

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
-const { Sequelize } = require("sequelize");
+const { Sequelize, DataTypes } = require("sequelize");
 
-const sequelize = new Sequelize("MSLearnDB", "sa", "password", {
+const sequelize = new Sequelize("ExpressWithMS", "sa", "password", {
   host: "localhost",
   port: 53687,
   dialect: "mssql",
@@ -15,6 +15,12 @@ try {
   console.error("Unable to connect to the database:", error);
 }
 
+const User = require("./models/User")(sequelize, DataTypes);
+
+(async () => {
+  await sequelize.sync();
+})();
+
 // // for create and update. Body Parser
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public")); // for public
@@ -22,11 +28,11 @@ app.use(express.static("public")); // for public
 const methodOverride = require("method-override");
 app.use(methodOverride("_method"));
 
-const gameController = require("./controllers/gameController");
-app.use("/api/games", gameController);
+// const gameController = require("./controllers/gameController");
+// app.use("/api/games", gameController);
 
-app.get("/", (req, res) => {
-  res.redirect("/api/games");
+app.get("/", async (req, res) => {
+  const jane = await User.create({ name: "Jane" });
 });
 
 app.listen(3500, () => {
@@ -34,4 +40,58 @@ app.listen(3500, () => {
   console.log("http://localhost:3500/");
 });
 
-// "SqlConnection": "Data Source=DESKTOP-9DKU259;Initial Catalog=GAMEDB;Integrated Security=True;"
+//=====================================================================================================================
+
+// const express = require("express");
+// const app = express();
+// const { Sequelize, Model, DataTypes } = require("sequelize");
+// const User = require("./models/User.js");
+
+// const sequelize = new Sequelize("ExpressWithMS", "sa", "password", {
+//   host: "localhost",
+//   port: 53687,
+//   dialect: "mssql",
+// });
+
+// try {
+//   sequelize.authenticate();
+//   console.log("Connection has been established successfully.");
+// } catch (error) {
+//   console.error("Unable to connect to the database:", error);
+// }
+
+// // const User = sequelize.define("user", {
+// //   name: DataTypes.TEXT,
+// //   favoriteColor: {
+// //     type: DataTypes.TEXT,
+// //     defaultValue: "green",
+// //   },
+// //   age: DataTypes.INTEGER,
+// //   cash: DataTypes.INTEGER,
+// // });
+
+// (async () => {
+//   await sequelize.sync();
+//   // Code here
+// })();
+
+// // // for create and update. Body Parser
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.static("public")); // for public
+// // for delete.
+// const methodOverride = require("method-override");
+// app.use(methodOverride("_method"));
+
+// // const gameController = require("./controllers/gameController");
+// // app.use("/api/games", gameController);
+
+// app.get("/", async (req, res) => {
+//   const jane = await User.create({ name: "Jane" });
+//   console.log("Jane's auto-generated ID:", jane.id);
+//   // res.redirect("/api/games");
+// });
+
+// app.listen(3500, () => {
+//   console.log("Start liao!");
+//   console.log("http://localhost:3500/");
+// });
